d9a306a6deeb8df96d8a308469289aac0c5fdfce
Juan Pablo Mayagoitia Franco <103715117+jpmfranco@users.noreply.github.com>
2025-07-11 17:46:21 -0600

    day 3 JS

--- JavaScript/JS/ejercicioDOM.js
+++ JavaScript/JS/ejercicioDOM.js
@@ -3,10 +3,33 @@
 window.addEventListener("load",function(){
     console.log("dom cargado");
 
-    var form = document.querySelector("#formulario");
-    form.addEventListener("submit",function(){
+    var formulario = document.querySelector("#form");
+    var bd = document.querySelector(".dashed");
+    bd.style.display = "none";
+    formulario.addEventListener("submit",function(){
         console.log("Evento submit capturado");
+        var nombre = document.querySelector("#nombre").value;
+        var apellidos = document.querySelector("#apellidos").value;
+        var edad = document.querySelector("#edad").value;
+        bd.style.display = "block";
+        // var data  = [nombre,apellidos,edad];
+        // var i;
+        // for(i in data){
+        //     var p = document.createElement("p");
+        //     p.append(data[i]);
+        //     bd.append(p)
+        // }
+        var pn = document.querySelector("#pn span");
+        var pa = document.querySelector("#pa span");
+        var pe = document.querySelector("#pe span");
+        pn.innerHTML = nombre;
+        pa.innerHTML = apellidos;
+        pe.innerHTML = edad;
 
-        return false;
+
+
+
+
+
     });
 });
